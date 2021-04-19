44fbb4b27a71741b997d63a6ae65736630594cc4
Umar <enggomarpu@gmail.com>
2021-04-19 17:37:10 +0500

    service worker edite

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -11,7 +11,7 @@
 // opt-in, read https://bit.ly/CRA-PWA
 // Import Pushy Service Worker 1.0.7
 
-importScripts('https://sdk.pushy.me/web/1.0.7/pushy-service-worker.js');
+//importScripts('https://sdk.pushy.me/web/1.0.7/pushy-service-worker.js');
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
